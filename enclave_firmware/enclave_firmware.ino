7087d128c640866763b5eba414375a92ba78123e
Haifisch <laws.ra1n@me.com>
2016-09-24 00:07:24 -0700

    Keychain Sec Overview added

--- enclave_firmware/enclave_firmware.ino
+++ enclave_firmware/enclave_firmware.ino
@@ -9,26 +9,23 @@
 OS_STK     taskA_stk[TASK_STK_SIZE];   
 OS_STK     btle_stk[TASK_STK_SIZE];   
 OS_STK     cnsl_stk[TASK_STK_SIZE];   
+OS_STK     aesinit_stk[TASK_STK_SIZE];   
 
 OS_TID TaskMId;
 SerialCommand sCmd; 
-
-void  initEnclaveOS(void* pdata) {
-  Serial.println("=====================================");
-  Serial.println("::Enclave Firmware");
-  Serial.println("::Copyright Enclave Security LLC 2016");
-  Serial.println("::BUILD_DATE: " __DATE__);
-  Serial.println("::BUILD_TAG: DEBUG");
-  Serial.println("=====================================");
-  Serial.println("Testing crypto...");
-
-  CoExitTask();
-}
 
 void initBTStack(void *pdata) {
   Serial.println("Initializing BT stack...");
   // init BTLE stack
   Serial.println("BTLE Init done.");
+  
+  CoExitTask();
+}
+
+void initAESStack(void *pdata) {
+  Serial.println("Initializing AES stack...");
+  // init BTLE stack
+  Serial.println("AES Init done.");
   
   CoExitTask();
 }
@@ -39,6 +36,7 @@ void initConsole(void *pdata) {
      sCmd.addCommand("iamnotapplediagsdammit",    iamnotapplediags); 
   }
   sCmd.addCommand("reset",    resetDevice); 
+  sCmd.addCommand("version",  printVersionHeader); 
   sCmd.setDefaultHandler(unrecognized);  
   
   Serial.println(":-)");
@@ -64,20 +62,35 @@ void unrecognized(const char *command) {
   Serial.println("\"");
 }
 
+void printVersionHeader() {
+  Serial.println("=====================================");
+  Serial.println("::Enclave Firmware");
+  Serial.println("::Copyright Enclave Security LLC 2016");
+  Serial.println("::BUILD_DATE: " __DATE__);
+  Serial.println("::BUILD_TAG: DEBUG");
+  Serial.println("=====================================");
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(pinLED, OUTPUT);
   Serial.println("Initializing firmware...");
-  CoInitOS();
-  CoCreateTask(initEnclaveOS,0,0,&taskA_stk[TASK_STK_SIZE-1],TASK_STK_SIZE);
+  printVersionHeader();
   
+  CoInitOS();
   TaskMId = CoCreateTask(initBTStack,0,1,&btle_stk[TASK_STK_SIZE-1],TASK_STK_SIZE);
   if (TaskMId == E_CREATE_FAIL) {
     Serial.println("Failed to create BT stack task");
     while (1); // hang
   }
+
+  TaskMId = CoCreateTask(initAESStack,0,2,&aesinit_stk[TASK_STK_SIZE-1],TASK_STK_SIZE);
+  if (TaskMId == E_CREATE_FAIL) {
+    Serial.println("Failed to create BT stack task");
+    while (1); // hang
+  }
   
-  TaskMId = CoCreateTask(initConsole,0,2,&cnsl_stk[TASK_STK_SIZE-1],TASK_STK_SIZE);
+  TaskMId = CoCreateTask(initConsole,0,3,&cnsl_stk[TASK_STK_SIZE-1],TASK_STK_SIZE);
   if (TaskMId == E_CREATE_FAIL) {
     Serial.println("Failed to create console stack task");
     while (1); // hang
